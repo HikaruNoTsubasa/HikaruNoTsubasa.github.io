5d1380716acb22e39623cc02c054b04193fcac85
Hikaru <kohikaru@gmail.com>
2019-09-19 04:09:45 +0800

    [feat] animation effect add

--- js/main.js
+++ js/main.js
@@ -7,20 +7,20 @@ class RichForYou {
       countdownTime: 10
     };
     this.bless = [{
-      method: '越施越富',
-      sp: '財寶天王',
+      method: '想要增長福德、壽命、智慧',
+      sp: '每天聽此咒，可累積無數善緣及財富！<br>「嗡 臧巴拉 雜列 扎耶 梭哈」',
       videoId: 'pLFNEsBY8Rs'
     }, {
-      method: '廣結善緣',
-      sp: '作明佛母',
+      method: '想要增加好人緣',
+      sp: '服務大家的事情，廣結善緣的事情要多做，不要一點福都沒有了，還在那裡嬌縱，孤芳自賞。覺得好像自己很了不起，你不跟我互動，我才不屑跟你互動，還傻到這樣，你都已經沒福了，沒有福報。',
       videoId: '18k1ljJ0SnA'
     }, {
-      method: '大開智慧',
-      sp: '文殊菩薩',
+      method: '想要開啟智慧',
+      sp: '搞不清楚就不叫智慧，在那邊模模糊糊的模稜兩可，我知道，可是我做不到，那叫沒有智慧。有智慧，你看文殊菩薩代表智慧的大菩薩，劍都指著自已的頭，對不對？當斷則斷，沒有那邊藕斷絲連的，該怎麼做，就怎麼做，非常的清楚，非常的分明。',
       videoId: 'C-LXky2LT5o'
     }, {
-      method: '長壽健康',
-      sp: '長壽佛',
+      method: '想要長壽健康',
+      sp: '唯有戒殺護生',
       videoId: 'o67Ui8khQjk'
     }];
 
@@ -44,12 +44,18 @@ class RichForYou {
     const maxNumber = this.bless.length;
     const _bless = this.bless[indexRandom(maxNumber)];
     $('#js-answerTitle').text(_bless.method);
-    $('#js-answerDesc').text(`${_bless.sp}加持`);
-    let player = new YT.Player('ytplayer', {
-      height: '360',
-      width: '640',
-      videoId: _bless.videoId
-    });
+    $('#js-answerDesc').html(`${_bless.sp}`);
+
+    try {
+      let player = new YT.Player('ytplayer', {
+        height: '360',
+        width: '640',
+        videoId: _bless.videoId
+      });
+    } catch (msg) {
+      console.log('YT api include error.');
+    }
+
     $('#subscribe').on('click', e => {
       e.preventDefault();
       this.saveToGdrive('subscribe');
@@ -62,14 +68,13 @@ class RichForYou {
     switch (status) {
       case 'entry':
         $('#countdown').show();
-        $('#signing').addClass('active');
+        $('#signing').removeClass('active');
         $('#signStart').prop('disabled', true);
         $('#js-blessing').addClass('active').fadeIn('fast');
         break;
 
       default:
         $('#countdown').hide();
-        $('#signing').removeClass('active');
         $('#js-blessing').removeClass('active').fadeOut('slow');
         let fancyboxDefaults = $.fancybox.defaults;
         fancyboxDefaults.animationEffect = "zoom-in-out";
@@ -79,6 +84,7 @@ class RichForYou {
           opts: {
             afterClose: (instance, current) => {
               $('#signStart').prop('disabled', false);
+              $('#signing').addClass('active');
             }
           }
         });
@@ -143,9 +149,10 @@ class RichForYou {
   }
 
   main() {
-    // this.devSet()
+    this.devSet();
     this.startGame();
-    this.gameAnswer(); // this.devMode()
+    this.gameAnswer();
+    this.devMode();
   }
 
 }
