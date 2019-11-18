92e0e48ce5ca4a62da0a9d8b0ddb1faf7480b6ec
Hikaru <kohikaru@gmail.com>
2019-11-19 03:40:59 +0800

    demo

--- js/everyday.js
+++ js/everyday.js
@@ -1,1 +1,323 @@
-"use strict";class DayBless{constructor(e){this.global=e,this.states={countdownTime:5},this.sets={today:moment(),manjushriDays:{"初一":"受福-吉","初二":"官事","初三":"歡喜-吉","初四":"富貴-吉","初五":"面黑","初六":"神通-吉","初七":"康寧-吉","初八":"壽長-吉","初九":"聞法-吉","初十":"記別-吉","十一":"眼明-吉","十二":"困苦","十三":"少白","十四":"神定-吉","十五":"大吉-吉","十六":"向益-吉","十七":"多病","十八":"犯盜","十九":"悟道-吉","二十":"禍崇","二一":"患難","二二":"受俱-吉","二三":"多聞-吉","二四":"證果-吉","二五":"鬥諍","二六":"祥瑞-吉","二七":"瘡癬","二八":"受寃","二九":"長慧-吉","三十":"如意-吉"},chtDay:{Sun:"日",Mon:"一",Tue:"二",Wed:"三",Thu:"四",Fri:"五",Sat:"六"}}}calBbuild(){const e=this.sets.today.format("YYYY-MM-DD"),t="calRender",s=`https://spreadsheets.google.com/tq?tqx=responseHandler:${t}&tq=${encodeURI(`select * where A = date '${e}'`)}&key=1TMZHX9LRmb6ROdXsysHY5hKFubWuM2klz51xLOGYSLE&gid=0`;$.getScript(s),this.global[t]=(e=>{const t=this.sets.today,s=this.sets.chtDay[t.format("ddd")],a=e.table.rows[0].c,o=a[1].v,i=this.sets.manjushriDays[o.slice(o.length-2)];if($("#js-date").text(t.format("YYYY.MM.DD")+"("+s+")").fitText(1.05),$(".bless-title").fitText(.9),$(".cal-lunar__val").text(o),i.indexOf("-")>-1?$(".cal-manjushri__val").addClass("good").text(i.slice(0,2)):$(".cal-manjushri__val").text(i),null!==a[2]&&null!==a[2].v){const e=a[2].v.split(",");for(const t of e)$(".cal-spDay").append(`<div class="item">${t}</div>`)}if(null!==a[3]&&null!==a[3].v){const e=a[3].v.split(",");for(const t of e)$(".buddha-info .list").append(`<li><div class="item">${t}</div></li>`)}$("#js-preToday").text(this.sets.today.format("MM.DD"))})}gameAnswer(){const e=this.bless.length,t=this.bless[function(e,t=0){return Math.floor(Math.random()*(+e-+t))+ +t}(e)];$("#js-fortune").text(t.fortune),$("#js-answerDestSub").html(t.sp);const s={height:360,width:640,videoId:t.videoId};t.videoStart?s.startSeconds=t.videoStart:s.startSeconds=0;try{let t;t=new YT.Player("ytplayer",{videoId:s.videoId,startSeconds:s.startSeconds,playerVars:{start:s.startSeconds},events:{onReady:e=>{t.playVideo()}}})}catch(e){const t=`<iframe width="${s.width}" height="${s.height}" src="https://www.youtube.com/embed/${s.videoId}?start=${s.startSeconds}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;$(".answer-mv").append(t)}$("#subscribe").on("click",e=>{e.preventDefault();const s=$(e.currentTarget).data("href");$(e.currentTarget).prop("disabled",!0),this.saveToGdrive("subscribe",s,t.fortune)})}gameBody(e="entry"){switch(e){case"entry":$("#countdown").show(),$("#signing").removeClass("active"),$("#signStart").prop("disabled",!0),$("#js-blessing").addClass("active").fadeIn("fast");break;default:$("#countdown").hide(),$("#js-blessing").removeClass("active").fadeOut("slow"),$.fancybox.defaults.animationEffect="zoom-in-out",$.fancybox.open({src:"#js-answer",type:"inline",opts:{afterClose:(e,t)=>{$("#signStart").prop("disabled",!1),$("#signing").addClass("active")}}})}}startGame(){$("#signStart").on("click",e=>{e.preventDefault();const t=this.gameBody;t(),this.saveToGdrive("play");let s=1e3*this.states.countdownTime;!function e(){s<=0?t("back"):setTimeout(e,1e3),s-=1e3}()})}saveToGdrive(e,t,s){const a=(()=>{try{return this.guestIpObj.responseJSON.ip}catch(e){return""}})(),[o,i]=(()=>FRUBIL?[FRUBIL.client.name,FRUBIL.client.os]:["error","can not import FRUBIL"])(),r={type:"post",url:"https://script.google.com/macros/s/AKfycbxgfyVf9xXWTEVz7ck_lLcIqhlE7MTb159wYbKq_mhPCqhOwh2j/exec",data:{method:"write",time:this.guestTime,ip:a,browser:o,status:e,note:`${i}`}};"subscribe"===e&&(r.complete=(()=>{(e=>{window.location.href=e})(t)}),r.data.note=`${s}: ${i}`),$.ajax(r)}btnBind(){$(".btn--bless").on("click",()=>{$("body").addClass("day"),this.dayView()}),$("#subscribe").on("click",e=>{e.preventDefault(),$(e.currentTarget).data("href"),$(e.currentTarget).prop("disabled",!0)})}dayView(){const e="blessRender",t=`https://spreadsheets.google.com/tq?tqx=responseHandler:${e}&tq=${encodeURI(`select * where A = ${function(e,t=1){return Math.floor(Math.random()*(10-+t))+ +t}()}`)}&key=1TMZHX9LRmb6ROdXsysHY5hKFubWuM2klz51xLOGYSLE&gid=307467837`;$.getScript(t),this.global[e]=(e=>{const t=e.table.rows[0].c,[s,a]=[t[1].v||90,t[2].v.split(",")||"今天的您，看來事事得心應手、順心如意、所求皆能如意滿願：),想辦法讓自己的心很穩定，不要心隨萬境波,趁有福報的時候，應該搶時間修福，要時時提醒自己打從我們從娘胎出世，無常就在逼著我們。,無常來了，順境一彈指剎那之間就會變成大逆境！<br>我們要修擺脫三惡道的業力習氣，不要很茫茫然的過日子，要積極的成辦自他二利。"],o={videoId:t[4]||"4ozNk1G4Ao8",width:"100%",height:300,startSeconds:t[5]||0};let i="";const r=`inset ${.4*s}px -10px #eccc68`;$("#js-blessMoon").css({"box-shadow":r}),$("#js-blessNum").text(s);for(const e of a)i+=`<p>${e}</p>`;$("#js-blessContent").append(i),anime.timeline({easing:"easeOutElastic(1, .8)"}).add({targets:"#js-blessMoon",keyframes:[{opacity:1,duration:750,translateY:-10},{translateY:0,duration:250}],delay:1e3}).add({targets:"#js-blessNum",keyframes:[{fontSize:"3em",duration:750},{fontSize:"1em",duration:250}]},1e3).add({targets:".bless__main p",top:0,duration:250,opacity:1,delay:anime.stagger(300),easing:"linear"});try{let t;t=new YT.Player("ytplayer",{videoId:o.videoId,playerVars:{start:o.startSeconds},events:{onReady:e=>{console.log(e),t.playVideo()}}})}catch(e){const t=`<iframe width="${o.width}" height="${o.height}" src="https://www.youtube.com/embed/${o.videoId}?start=${o.startSeconds}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;$("#ytplayer").append(t)}})}devMode(){$(".btn--bless").trigger("click")}main(){this.calBbuild(),this.btnBind()}}window.addEventListener("load",e=>{$("#js-loader").fadeOut("slow"),$("#wrap").css("visibility","visible"),new DayBless(window).main()});
+"use strict";
+
+class DayBless {
+  constructor(global) {
+    this.global = global;
+    this.states = {
+      countdownTime: 5
+    };
+    this.sets = {
+      today: moment(),
+      manjushriDays: {
+        初一: '受福-吉',
+        初二: '官事',
+        初三: '歡喜-吉',
+        初四: '富貴-吉',
+        初五: '面黑',
+        初六: '神通-吉',
+        初七: '康寧-吉',
+        初八: '壽長-吉',
+        初九: '聞法-吉',
+        初十: '記別-吉',
+        十一: '眼明-吉',
+        十二: '困苦',
+        十三: '少白',
+        十四: '神定-吉',
+        十五: '大吉-吉',
+        十六: '向益-吉',
+        十七: '多病',
+        十八: '犯盜',
+        十九: '悟道-吉',
+        二十: '禍崇',
+        二一: '患難',
+        二二: '受俱-吉',
+        二三: '多聞-吉',
+        二四: '證果-吉',
+        二五: '鬥諍',
+        二六: '祥瑞-吉',
+        二七: '瘡癬',
+        二八: '受寃',
+        二九: '長慧-吉',
+        三十: '如意-吉'
+      },
+      chtDay: {
+        Sun: '日',
+        Mon: '一',
+        Tue: '二',
+        Wed: '三',
+        Thu: '四',
+        Fri: '五',
+        Sat: '六'
+      }
+    };
+
+    this.guestIpObj = (() => {
+      const ipifyUrl = 'https://api.ipify.org?format=jsonp&callback=?';
+      return $.getJSON(ipifyUrl);
+    })();
+  }
+
+  calBbuild() {
+    const todayStr = this.sets.today.format('YYYY-MM-DD');
+    const calDbSet = {
+      baseUrl: 'https://spreadsheets.google.com/tq?',
+      key: '1TMZHX9LRmb6ROdXsysHY5hKFubWuM2klz51xLOGYSLE',
+      gid: 0,
+      sql: encodeURI(`select * where A = date '${todayStr}'`),
+      callback: 'calRender'
+    };
+    const calDbUrl = `${calDbSet.baseUrl}tqx=responseHandler:${calDbSet.callback}&tq=${calDbSet.sql}&key=${calDbSet.key}&gid=${calDbSet.gid}`;
+    $.getScript(calDbUrl);
+
+    this.global[calDbSet.callback] = json => {
+      const today = this.sets.today;
+      const chtDay = this.sets.chtDay[today.format('ddd')];
+      const jsonrow = json.table.rows[0].c;
+      const lunarStr = jsonrow[1].v;
+      const manjushriValue = this.sets.manjushriDays[lunarStr.slice(lunarStr.length - 2)];
+      $('#js-date').text(today.format('YYYY.MM.DD') + '(' + chtDay + ')').fitText(1.05);
+      $('.bless-title').fitText(0.9);
+      $('.cal-lunar__val').text(lunarStr);
+
+      if (manjushriValue.indexOf('-') > -1) {
+        $('.cal-manjushri__val').addClass('good').text(manjushriValue.slice(0, 2));
+      } else {
+        $('.cal-manjushri__val').text(manjushriValue);
+      }
+
+      if (jsonrow[2] !== null && jsonrow[2].v !== null) {
+        const spArr = jsonrow[2].v.split(',');
+
+        for (const spValue of spArr) {
+          $('.cal-spDay').append(`<div class="item">${spValue}</div>`);
+        }
+      }
+
+      if (jsonrow[3] !== null && jsonrow[3].v !== null) {
+        const blessArr = jsonrow[3].v.split(',');
+
+        for (const blessValue of blessArr) {
+          $('.buddha-info .list').append(`<li><div class="item">${blessValue}</div></li>`);
+        }
+      }
+
+      $('#js-preToday').text(this.sets.today.format('MM.DD'));
+    };
+  }
+
+  saveToGdrive(guestStatus, subscribeUrl, fortune) {
+    ;
+
+    const _ip = (() => {
+      try {
+        return this.guestIpObj.responseJSON.ip;
+      } catch (msg) {
+        return '';
+      }
+    })();
+
+    const [_browser, _note] = (() => {
+      if (FRUBIL) {
+        return [FRUBIL.client.name, FRUBIL.client.os];
+      }
+
+      return ['error', 'can not import FRUBIL'];
+    })();
+
+    const toSubscribe = url => {
+      window.location.href = url;
+    };
+
+    const ajaxOpts = {
+      type: 'post',
+      url: 'https://script.google.com/macros/s/AKfycbx20Lla6yjYC26h7QDNDGv5tTZ-Mft1EhAIWVqYotrtXxjm85Pk/exec',
+      data: {
+        method: 'write',
+        time: this.sets.today.format('YYYY-MM-DD hh:mm:ss'),
+        ip: _ip,
+        browser: _browser,
+        status: guestStatus,
+        note: `${_note}`
+      }
+    };
+
+    if (guestStatus === 'subscribe') {
+      ajaxOpts.complete = () => {
+        toSubscribe(subscribeUrl);
+      };
+
+      ajaxOpts.data.note = `${fortune}: ${_note}`;
+    }
+
+    $.ajax(ajaxOpts);
+  }
+
+  btnBind() {
+    $('.btn--bless').on('click', () => {
+      $('body').addClass('day');
+      this.dayView();
+    });
+    $('#subscribe').on('click', e => {
+      e.preventDefault();
+
+      const _url = $(e.currentTarget).data('href');
+
+      $(e.currentTarget).prop('disabled', true);
+      this.saveToGdrive('subscribe', _url, $('#js-blessNum').text());
+    });
+  }
+
+  dayView() {
+    const fadeTime = 800;
+
+    const indexRandom = function indexRandom(maxNumber, minNumber = 1) {
+      return Math.floor(Math.random() * (+maxNumber - +minNumber)) + +minNumber;
+    };
+
+    const blessDbSet = {
+      baseUrl: 'https://spreadsheets.google.com/tq?',
+      key: '1TMZHX9LRmb6ROdXsysHY5hKFubWuM2klz51xLOGYSLE',
+      gid: 307467837,
+      sql: encodeURI(`select * where A = ${indexRandom(10)}`),
+      callback: 'blessRender'
+    };
+    const blessDbUrl = `${blessDbSet.baseUrl}tqx=responseHandler:${blessDbSet.callback}&tq=${blessDbSet.sql}&key=${blessDbSet.key}&gid=${blessDbSet.gid}`;
+    $.getScript(blessDbUrl);
+
+    this.global[blessDbSet.callback] = json => {
+      const dayViewData = json.table.rows[0].c;
+      const [_percent, _blessContent] = [dayViewData[1].v || 90, dayViewData[2].v.split(',') || '今天的您，看來事事得心應手、順心如意、所求皆能如意滿願：),想辦法讓自己的心很穩定，不要心隨萬境波,趁有福報的時候，應該搶時間修福，要時時提醒自己打從我們從娘胎出世，無常就在逼著我們。,無常來了，順境一彈指剎那之間就會變成大逆境！<br>我們要修擺脫三惡道的業力習氣，不要很茫茫然的過日子，要積極的成辦自他二利。'];
+      const ytOpts = {
+        videoId: dayViewData[4].v || '4ozNk1G4Ao8',
+        width: '100%',
+        height: 300,
+        startSeconds: dayViewData[5].v || 0
+      };
+      let player;
+      let blessContentHtml = '';
+
+      const _moonPerc = (() => {
+        const moonMinValue = 40 / 100;
+        const exportMoonValue = moonMinValue * _percent;
+        return `inset ${exportMoonValue}px -10px #eccc68`;
+      })();
+
+      $('#js-blessMoon').css({
+        'box-shadow': _moonPerc
+      });
+      $('#js-blessNum').text(_percent);
+
+      for (const item of _blessContent) {
+        blessContentHtml += `<p>${item}</p>`;
+      }
+
+      $('#js-blessContent').append(blessContentHtml);
+      anime.timeline({
+        easing: 'easeOutElastic(1, .8)',
+        begin: () => {
+          try {
+            function onYouTubeIframeAPIReady() {
+              player = new YT.Player('ytplayer', {
+                videoId: ytOpts.videoId,
+                playerVars: {
+                  start: ytOpts.startSeconds
+                } // events: {
+                //     onReady: event => {
+                //     }
+                // }
+
+              });
+            }
+
+            onYouTubeIframeAPIReady();
+          } catch (msg) {
+            const videoTmp = `<iframe width="${ytOpts.width}" height="${ytOpts.height}" src="https://www.youtube.com/embed/${ytOpts.videoId}?start=${ytOpts.startSeconds}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            $('#ytplayer').append(videoTmp);
+          }
+        },
+        complete: () => {
+          anime.timeline({
+            complete: () => {
+              player.playVideo();
+            }
+          }).add({
+            targets: '.wrap--day .page-footbox',
+            height: '20.5vh',
+            begin: () => {
+              $('.page-footbox').addClass('active');
+            }
+          }).add({
+            targets: '.wrap--day .ytbox, .wrap--day .ripple',
+            opacity: 1
+          });
+        }
+      }).add({
+        targets: '#js-blessMoon',
+        keyframes: [{
+          opacity: 1,
+          duration: 750,
+          translateY: -10
+        }, {
+          translateY: 0,
+          duration: 250
+        }],
+        delay: 1000
+      }).add({
+        targets: '#js-blessNum',
+        keyframes: [{
+          fontSize: '3em',
+          duration: 750
+        }, {
+          fontSize: '1em',
+          duration: 250
+        }]
+      }, 1000).add({
+        targets: '.bless__main p',
+        top: 0,
+        duration: 250,
+        opacity: 1,
+        delay: anime.stagger(300),
+        easing: 'linear'
+      });
+    };
+  } // devSet() {
+  //     this.states.countdownTime = 1
+  // }
+
+
+  devMode() {
+    //     $('#signStart').off('click').on('click', e => {
+    //         e.preventDefault()
+    //         const gameBody = this.gameBody
+    //         gameBody()
+    //         // this.saveToGdrive('play')
+    //         let countdownTime = 1000 * this.states.countdownTime // 倒數 n 秒
+    //         function MyCounter() {
+    //             if(countdownTime <= 0) {
+    //                 gameBody('back')
+    //             } else {
+    //                 setTimeout(MyCounter, 1000)
+    //             }
+    //             countdownTime -= 1000
+    //         }
+    //         // MyCounter()
+    //     })
+    $('.btn--bless').trigger('click');
+  }
+
+  main() {
+    // this.devSet()
+    // this.startGame()
+    // this.gameAnswer()
+    this.calBbuild();
+    this.btnBind(); // this.devMode()
+  }
+
+}
+
+window.addEventListener('load', event => {
+  $('#js-loader').fadeOut('slow');
+  $('#wrap').css('visibility', 'visible');
+  const blessObj = new DayBless(window);
+  blessObj.main();
+});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,246 @@
-"use strict";class RichForYou{constructor(e){this.jqStart=$(e),this.states={countdownTime:5},this.bless=[{fortune:"上上籤",sp:"✨🎉🎊2020年無論在事業、工作、人際關係...各方面都能得心應手，做什麼事情都能如魚得水，有很多的善緣、貴人會挺力相助。<br><br>2020年看來，也是忙碌充實的一年，這麼多緣境，要如何做到面面俱到、順利過關？以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"PZfwbe7wdxg"},{fortune:"大吉籤",sp:"✨🎉2020年是成果豐碩的一年，人生具有代表性、關鍵性的指標在今年可以順利建立。立定好志向，放手一搏吧！有些一直改不了的壞習慣毛病、糾結很久的困境，今年都能正式告別解決。<br><br>2020年也會讓您忙得非常有功德。要多多行善、多關心家人。注意自己身體健康、維持規律的生活。 以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"K0TCQgRVe0k"},{fortune:"上吉籤",sp:"✨2020年是立定志向一年。面對很多緣境，不要忘記如何去取捨，好的我們應該擷取它，不好的應立即斷除遠離捨棄！<br><br>2020年新的一年到來，好好檢視自身，把那些積累很久的陳年問題，積極明快的面對，您將會有個嶄新、改頭換面的自己。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"wGrkrwIXs3w"},{fortune:"中吉籤",sp:"🎇2020年是勇敢拚搏的一年。很多事情不要流於形式要有積極的進取心，不要迷失。心裡要有明確的方向，堅定持續的去做，不用再擔憂而躊躇不前了，一切都是最好的安排，一切將會有截然不同的局面！<br><br>一心想改變命運之人，不會浪費時間在抱怨上。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"pDn1HGoVZk8"},{fortune:"中平籤",sp:"🎊平順就是福！2020年腳踏實地、穩紮穩打的把基本功練好！無論是該補足的技能、該經營的人脈、該修補的關係，努力的積資淨障、廣結善緣吧！<br><br>2020年建議您不要好高騖遠，坐這山望那山，就依循您最初設定的目標，穩健的步伐向前邁去！當在您想要做些什麼改變的時候，會有些許違緣逆境來障礙，不要擔心，以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"f3H4xfKbwWA",videoStart:291},{fortune:"中下籤",sp:"🛠看到自己的問題，努力改變吧！2020年對您來說是勇敢面對自己問題的一年，會有許多緣境在2020年都相繼伴隨發生了，不斷地調整修補、懺悔業障。要相信『命由我作、福自己求』，只要能多行善積德，積極地和冤親債主求解冤釋結，一切會大不相同！<br><br>2020年看來，您要有比以往更多的耐心、同理心來面對處理生活中的大小事，以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"x4vJVvCqkgQ"},{fortune:"下籤",sp:"⚖2020年真誠懺悔、反觀自照的一年。可能會有許多的違緣障礙，所求感覺無法如願。很多一時的身心的痛苦都是正在受報，世界受到因果業報的真理是真實不虛，要追求幸福安樂，一定要了解這一層道理。種如是因、得如是果，想辦法讓自己的心越來越穩定吧！<br><br>2020年看來，是要積極的改變、時刻反觀自照的一年。趁來年還沒到來時，趕緊行善、培福、累積福慧二資糧。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"pwRxkfeR-_4"},{fortune:"小吉籤",sp:"✨看穿世情、逆向思考的一年。不要隨順外界的緣境，去想一想很多事情好像不是一般世間的這種眼光來看待、來推理的，要能夠看穿、看破世情世境，要能夠逆世流、逆向操作。世情世境有著逆向思考。然後，有獨到的見地眼光，那才是真智慧。<br><br>2020年看來，如果要一窺真理實相你必須把這世間的情境給顛倒過來你才能洞悉、才能體會。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"troxAZk1UNU",videoStart:5},{fortune:"上中籤",sp:"🛠檢視自心、反觀自照的一年。2020年要時常去檢視自己，生命中最重要的事情，能陪伴我們一生的究竟是什麼？<br>👨👩是最關心我們的親友？<br>💲是努力賺取累積的財富？<br>❤還是身體健康？<br>若您想得到幸福安樂、具義的人生，一定要在心靈做灌溉滋養、一定要耕耘您的心圖，否則用力用錯地方，到頭來還是一場空。<br><br>2020年要常常生發善念、要多做利他的事情。心才能光明，遠離晦暗，還能招感五種上首的福報。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！",videoId:"Xz_1wXNzgDc"}],this.guestTime=(()=>{const e=new Date,[t,s,r,o,a,i]=[e.getFullYear(),e.getMonth()+1,e.getDate(),e.getHours(),e.getMinutes(),e.getSeconds()];return`${t}-${s}-${r} ${o}:${a}:${i}`})(),this.guestIpObj=$.getJSON("https://api.ipify.org?format=jsonp&callback=?")}gameAnswer(){const e=this.bless.length,t=this.bless[function(e,t=0){return Math.floor(Math.random()*(+e-+t))+ +t}(e)];$("#js-fortune").text(t.fortune),$("#js-answerDestSub").html(t.sp);const s={height:360,width:640,videoId:t.videoId};t.videoStart?s.startSeconds=t.videoStart:s.startSeconds=0;try{let t;t=new YT.Player("ytplayer",{videoId:s.videoId,startSeconds:s.startSeconds,playerVars:{start:s.startSeconds},events:{onReady:e=>{t.playVideo()}}})}catch(e){const t=`<iframe width="${s.width}" height="${s.height}" src="https://www.youtube.com/embed/${s.videoId}?start=${s.startSeconds}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;$(".answer-mv").append(t)}$("#subscribe").on("click",e=>{e.preventDefault();const s=$(e.currentTarget).data("href");$(e.currentTarget).prop("disabled",!0),this.saveToGdrive("subscribe",s,t.fortune)})}gameBody(e="entry"){switch(e){case"entry":$("#countdown").show(),$("#signing").removeClass("active"),$("#signStart").prop("disabled",!0),$("#js-blessing").addClass("active").fadeIn("fast");break;default:$("#countdown").hide(),$("#js-blessing").removeClass("active").fadeOut("slow"),$.fancybox.defaults.animationEffect="zoom-in-out",$.fancybox.open({src:"#js-answer",type:"inline",opts:{afterClose:(e,t)=>{$("#signStart").prop("disabled",!1),$("#signing").addClass("active")}}})}}startGame(){$("#signStart").on("click",e=>{e.preventDefault();const t=this.gameBody;t(),this.saveToGdrive("play");let s=1e3*this.states.countdownTime;!function e(){s<=0?t("back"):setTimeout(e,1e3),s-=1e3}()})}saveToGdrive(e,t,s){const r=(()=>{try{return this.guestIpObj.responseJSON.ip}catch(e){return""}})(),[o,a]=(()=>FRUBIL?[FRUBIL.client.name,FRUBIL.client.os]:["error","can not import FRUBIL"])(),i={type:"post",url:"https://script.google.com/macros/s/AKfycbxgfyVf9xXWTEVz7ck_lLcIqhlE7MTb159wYbKq_mhPCqhOwh2j/exec",data:{method:"write",time:this.guestTime,ip:r,browser:o,status:e,note:`${a}`}};"subscribe"===e&&(i.complete=(()=>{(e=>{window.location.href=e})(t)}),i.data.note=`${s}: ${a}`),$.ajax(i)}main(){this.startGame(),this.gameAnswer()}}$(()=>{new RichForYou("#start-box").main()}),window.addEventListener("load",e=>{$("#js-loader").fadeOut("slow"),$("#wrap").css("visibility","visible")});
+"use strict";
+
+class RichForYou {
+  constructor(jqStart) {
+    this.jqStart = $(jqStart);
+    this.states = {
+      countdownTime: 5
+    };
+    this.bless = [{
+      fortune: '上上籤',
+      sp: '✨🎉🎊2020年無論在事業、工作、人際關係...各方面都能得心應手，做什麼事情都能如魚得水，有很多的善緣、貴人會挺力相助。<br><br>2020年看來，也是忙碌充實的一年，這麼多緣境，要如何做到面面俱到、順利過關？以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'PZfwbe7wdxg'
+    }, {
+      fortune: '大吉籤',
+      sp: '✨🎉2020年是成果豐碩的一年，人生具有代表性、關鍵性的指標在今年可以順利建立。立定好志向，放手一搏吧！有些一直改不了的壞習慣毛病、糾結很久的困境，今年都能正式告別解決。<br><br>2020年也會讓您忙得非常有功德。要多多行善、多關心家人。注意自己身體健康、維持規律的生活。 以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'K0TCQgRVe0k'
+    }, {
+      fortune: '上吉籤',
+      sp: '✨2020年是立定志向一年。面對很多緣境，不要忘記如何去取捨，好的我們應該擷取它，不好的應立即斷除遠離捨棄！<br><br>2020年新的一年到來，好好檢視自身，把那些積累很久的陳年問題，積極明快的面對，您將會有個嶄新、改頭換面的自己。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'wGrkrwIXs3w'
+    }, {
+      fortune: '中吉籤',
+      sp: '🎇2020年是勇敢拚搏的一年。很多事情不要流於形式要有積極的進取心，不要迷失。心裡要有明確的方向，堅定持續的去做，不用再擔憂而躊躇不前了，一切都是最好的安排，一切將會有截然不同的局面！<br><br>一心想改變命運之人，不會浪費時間在抱怨上。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'pDn1HGoVZk8'
+    }, {
+      fortune: '中平籤',
+      sp: '🎊平順就是福！2020年腳踏實地、穩紮穩打的把基本功練好！無論是該補足的技能、該經營的人脈、該修補的關係，努力的積資淨障、廣結善緣吧！<br><br>2020年建議您不要好高騖遠，坐這山望那山，就依循您最初設定的目標，穩健的步伐向前邁去！當在您想要做些什麼改變的時候，會有些許違緣逆境來障礙，不要擔心，以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'f3H4xfKbwWA',
+      videoStart: 291
+    }, {
+      fortune: '中下籤',
+      sp: '🛠看到自己的問題，努力改變吧！2020年對您來說是勇敢面對自己問題的一年，會有許多緣境在2020年都相繼伴隨發生了，不斷地調整修補、懺悔業障。要相信『命由我作、福自己求』，只要能多行善積德，積極地和冤親債主求解冤釋結，一切會大不相同！<br><br>2020年看來，您要有比以往更多的耐心、同理心來面對處理生活中的大小事，以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'x4vJVvCqkgQ'
+    }, {
+      fortune: '下籤',
+      sp: '⚖2020年真誠懺悔、反觀自照的一年。可能會有許多的違緣障礙，所求感覺無法如願。很多一時的身心的痛苦都是正在受報，世界受到因果業報的真理是真實不虛，要追求幸福安樂，一定要了解這一層道理。種如是因、得如是果，想辦法讓自己的心越來越穩定吧！<br><br>2020年看來，是要積極的改變、時刻反觀自照的一年。趁來年還沒到來時，趕緊行善、培福、累積福慧二資糧。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'pwRxkfeR-_4'
+    }, {
+      fortune: '小吉籤',
+      sp: '✨看穿世情、逆向思考的一年。不要隨順外界的緣境，去想一想很多事情好像不是一般世間的這種眼光來看待、來推理的，要能夠看穿、看破世情世境，要能夠逆世流、逆向操作。世情世境有著逆向思考。然後，有獨到的見地眼光，那才是真智慧。<br><br>2020年看來，如果要一窺真理實相你必須把這世間的情境給顛倒過來你才能洞悉、才能體會。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'troxAZk1UNU',
+      videoStart: 5
+    }, {
+      fortune: '上中籤',
+      sp: '🛠檢視自心、反觀自照的一年。2020年要時常去檢視自己，生命中最重要的事情，能陪伴我們一生的究竟是什麼？<br>👨👩是最關心我們的親友？<br>💲是努力賺取累積的財富？<br>❤還是身體健康？<br>若您想得到幸福安樂、具義的人生，一定要在心靈做灌溉滋養、一定要耕耘您的心圖，否則用力用錯地方，到頭來還是一場空。<br><br>2020年要常常生發善念、要多做利他的事情。心才能光明，遠離晦暗，還能招感五種上首的福報。以下是慈悲的 龍德上師對您明年運勢的一段珍貴開示，趕緊去看看吧！',
+      videoId: 'Xz_1wXNzgDc'
+    }];
+
+    this.guestTime = (() => {
+      const now = new Date();
+      const [_year, _month, _day, _hours, _minutes, _seconds] = [now.getFullYear(), now.getMonth() + 1, now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds()];
+      return `${_year}-${_month}-${_day} ${_hours}:${_minutes}:${_seconds}`;
+    })();
+
+    this.guestIpObj = (() => {
+      const ipifyUrl = 'https://api.ipify.org?format=jsonp&callback=?';
+      return $.getJSON(ipifyUrl);
+    })();
+  }
+
+  gameAnswer() {
+    const indexRandom = function indexRandom(maxNumber, minNumber = 0) {
+      return Math.floor(Math.random() * (+maxNumber - +minNumber)) + +minNumber;
+    };
+
+    const maxNumber = this.bless.length;
+    const _bless = this.bless[indexRandom(maxNumber)];
+    $('#js-fortune').text(_bless.fortune);
+    $('#js-answerDestSub').html(_bless.sp);
+    const ytOpts = {
+      height: 360,
+      width: 640,
+      videoId: _bless.videoId
+    };
+
+    if (_bless.videoStart) {
+      ytOpts.startSeconds = _bless.videoStart;
+    } else {
+      ytOpts.startSeconds = 0;
+    }
+
+    try {
+      let player;
+      onYouTubeIframeAPIReady();
+
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('ytplayer', {
+          videoId: ytOpts.videoId,
+          startSeconds: ytOpts.startSeconds,
+          playerVars: {
+            start: ytOpts.startSeconds
+          },
+          events: {
+            onReady: event => {
+              player.playVideo();
+            }
+          }
+        });
+      }
+    } catch (msg) {
+      const videoTmp = `<iframe width="${ytOpts.width}" height="${ytOpts.height}" src="https://www.youtube.com/embed/${ytOpts.videoId}?start=${ytOpts.startSeconds}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      $('.answer-mv').append(videoTmp);
+    }
+
+    $('#subscribe').on('click', e => {
+      e.preventDefault();
+
+      const _url = $(e.currentTarget).data('href');
+
+      $(e.currentTarget).prop('disabled', true);
+      this.saveToGdrive('subscribe', _url, _bless.fortune);
+    });
+  }
+
+  gameBody(status = 'entry') {
+    switch (status) {
+      case 'entry':
+        $('#countdown').show();
+        $('#signing').removeClass('active');
+        $('#signStart').prop('disabled', true);
+        $('#js-blessing').addClass('active').fadeIn('fast');
+        break;
+
+      default:
+        $('#countdown').hide();
+        $('#js-blessing').removeClass('active').fadeOut('slow');
+        let fancyboxDefaults = $.fancybox.defaults;
+        fancyboxDefaults.animationEffect = "zoom-in-out";
+        $.fancybox.open({
+          src: '#js-answer',
+          type: 'inline',
+          opts: {
+            afterClose: (instance, current) => {
+              $('#signStart').prop('disabled', false);
+              $('#signing').addClass('active');
+            }
+          }
+        });
+    }
+  }
+
+  startGame() {
+    $('#signStart').on('click', e => {
+      e.preventDefault();
+      const gameBody = this.gameBody;
+      gameBody();
+      this.saveToGdrive('play');
+      let countdownTime = 1000 * this.states.countdownTime // 倒數 n 秒
+      ;
+      !function MyCounter() {
+        if (countdownTime <= 0) {
+          gameBody('back');
+        } else {
+          setTimeout(MyCounter, 1000);
+        }
+
+        countdownTime -= 1000;
+      }();
+    });
+  }
+
+  saveToGdrive(guestStatus, subscribeUrl, fortune) {
+    ;
+
+    const _ip = (() => {
+      try {
+        return this.guestIpObj.responseJSON.ip;
+      } catch (msg) {
+        return '';
+      }
+    })();
+
+    const [_browser, _note] = (() => {
+      if (FRUBIL) {
+        return [FRUBIL.client.name, FRUBIL.client.os];
+      }
+
+      return ['error', 'can not import FRUBIL'];
+    })();
+
+    const toSubscribe = url => {
+      window.location.href = url;
+    };
+
+    const ajaxOpts = {
+      type: 'post',
+      url: 'https://script.google.com/macros/s/AKfycbxgfyVf9xXWTEVz7ck_lLcIqhlE7MTb159wYbKq_mhPCqhOwh2j/exec',
+      data: {
+        method: 'write',
+        time: this.guestTime,
+        ip: _ip,
+        browser: _browser,
+        status: guestStatus,
+        note: `${_note}`
+      }
+    };
+
+    if (guestStatus === 'subscribe') {
+      ajaxOpts.complete = () => {
+        toSubscribe(subscribeUrl);
+      };
+
+      ajaxOpts.data.note = `${fortune}: ${_note}`;
+    }
+
+    $.ajax(ajaxOpts);
+  } // devSet() {
+  //     this.states.countdownTime = 1
+  // }
+  // devMode() {
+  //     $('#signStart').off('click').on('click', e => {
+  //         e.preventDefault()
+  //         const gameBody = this.gameBody
+  //         gameBody()
+  //         // this.saveToGdrive('play')
+  //         let countdownTime = 1000 * this.states.countdownTime // 倒數 n 秒
+  //         function MyCounter() {
+  //             if(countdownTime <= 0) {
+  //                 gameBody('back')
+  //             } else {
+  //                 setTimeout(MyCounter, 1000)
+  //             }
+  //             countdownTime -= 1000
+  //         }
+  //         // MyCounter()
+  //     })
+  //     $('#signStart').trigger('click')
+  // }
+
+
+  main() {
+    // this.devSet()
+    this.startGame();
+    this.gameAnswer(); // this.devMode()
+  }
+
+}
+
+$(() => {
+  const richObj = new RichForYou('#start-box');
+  richObj.main();
+});
+window.addEventListener('load', event => {
+  $('#js-loader').fadeOut('slow');
+  $('#wrap').css('visibility', 'visible');
+});
